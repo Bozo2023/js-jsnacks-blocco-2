c9dbef25705f94602c5500276c0c9327e49cec7e
Bozo2023 <ulrob1.0@email.com>
2024-01-12 17:29:09 +0100

    snack2

---
+++ snack2/main.js
@@ -0,0 +1,12 @@
+let num=Random(1,100);
+function Random(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min)}
+    let NumGuest=parseInt(prompt("prova ad indovinare un numero da 1 a 100!"));
+    while(guess!==Random){
+        if(NumGuest>Random){
+            console.log("il numero è troppo alto:riprova di nuovo"  )
+        } else if(NumGuest<Random){
+            
+            
+        }
+    }
